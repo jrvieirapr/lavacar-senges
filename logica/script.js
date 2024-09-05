09d79e7bcb25a01a21158b431a1a901850bfe37a
jrvieirapr <105877022+jrvieirapr@users.noreply.github.com>
2024-09-05 11:54:08 -0300

    Finalizacao Logica e Inicio Tabela
    Tabela

--- logica/script.js
+++ logica/script.js
@@ -83,7 +83,7 @@ switch (diaDaSemana) {
 // laços de repetição
 let contador = 0
 // Enquanto meu teste for verdadeiro faça
-while(contador <= 10){
+while (contador <= 10) {
     console.log(contador);
     contador++; // incrementar:contardo = contador + 1;
 }
@@ -91,13 +91,66 @@ while(contador <= 10){
 let tentativa = 0;
 // EOF - end of file
 
-while(tentativa <5){
+while (tentativa < 5) {
     // await
-    console.log("Tentativa: "+tentativa)
+    console.log("Tentativa: " + tentativa)
     tentativa++;
 }
 
 // Quando eu ja sei o tamanho
+let target = 5;
+let incrementar = 0;
+let decrementar = 0;
+for (let i = 0; i <= target; i++) {
+    console.log("Passo: " + i);
+    console.log(incrementar++);
+    console.log(decrementar--);
+}
 
+// array coleção de dados
+let numeros = [10, 20, 30, 40, 50, 60];
 
+// foreach
+numeros.forEach(function (numero) {
+    console.log(numero);
+})
+// for(numero in numeros)
+// foreach(numeros as numero)
+numeros.forEach(n =>{console.log(n)})
+// Texto 'aspas simples' e "aspas duplas"
+// e entre `crases`
+let frutas  =['abacaxi', 'manga','uva','jabuticaba']
+frutas.forEach(f =>{console.log(f)})
+// Exemplo com crase
+frutas.forEach((f,i) =>{ 
+console.log(`${i} : ${f}`)
+})
 
+let tamanho = frutas.length
+console.log("Tamanho do array: "+tamanho)
+frutas[0] = "Gabiroba"
+console.log("Item: "+ frutas[0])
+
+frutas.push("Pitanga")
+console.log(frutas)
+frutas.pop()
+console.log(frutas)
+// Excluir um item de um array
+let filtro = frutas.filter( fruta => fruta !== "uva")
+console.log(filtro)
+// Primeiro e o indice do array, o segundo qtde
+let porIndice = frutas.splice(2,3)
+console.log(porIndice)
+
+// Exemplo Json ou dicionario
+let dicionario = {
+    nome: "Joao",
+    idade: "25",
+    cidade: "Senges"
+}
+let json = {
+    "nome": "Joao",
+    "idade": "25",
+    "cidade": "Senges"
+}
+console.log(dicionario.nome, json.nome)
